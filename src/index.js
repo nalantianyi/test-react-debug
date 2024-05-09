3df670f5ca3e9cefd87b604ae9839d53cfec4823
nalantianyi <103520146@qq.com>
2024-05-09 16:30:15 +0800

    feat: FunctionComponent的基本支持

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const jsx = (
-  <div>
-    <span>big react</span>
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <Child />
+    </div>
+  );
+}
+
+function Child() {
+  return <span>big react</span>;
+}
 
 const root = document.querySelector("#root");
-ReactDOM.createRoot(root).render(jsx);
+ReactDOM.createRoot(root).render(<App />);
 
 console.log(React);
-console.log(jsx);
+
 console.log(ReactDOM);
